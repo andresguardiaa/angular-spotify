4e6f4d63aa3bfb5a838198bd7f90fea29e3f9f9a
Guardia Espino <andres.guardia-espino@capgemini.com>
2025-05-23 10:34:28 +0200

    no hacía falta el appModule, Ejemplo de componente creado y configurado enapp.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,Routes } from '@angular/router';
 
-import { routes } from './app.routes';
+
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: EjemploComponent
+  }
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
