6d5b7143737f85fa73d62785e6427b1b998dea85
Guardia Espino <andres.guardia-espino@capgemini.com>
2025-05-26 09:30:42 +0200

    SideBar listo

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 //import { OnInit } from '@angular/core';
 
 @Component({
@@ -9,17 +10,63 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+ 
+  mainMenu: { defaultOptions: Array<any>, accessLink: Array<any>} = {defaultOptions: [], accessLink: []}
 
-  linksMenu: Array<any> = [
-    {
-      name: 'Home',
-      icon: 'uil-estate'
-    },
-    {
-      name: 'Search',
-      icon: 'uil-estate'
-    }
-  ]
+  customOptions: Array<any> = []
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/', 'auth']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' }
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+  }
 
 }
