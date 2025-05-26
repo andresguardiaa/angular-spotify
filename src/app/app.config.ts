a1954dc5542fb0d7deeacb55a5e6d0eb70a53bed
Guardia Espino <andres.guardia-espino@capgemini.com>
2025-05-26 10:30:09 +0200

    Generados layouts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,30 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 
  export const routes: Routes = [
     {
-      path: '',
-      loadComponent: () => import('./modules/home/home.component').then(m => m.HomeComponent)    
-    },
-    {
-      path:'tracks',
-      loadComponent: () => import('./modules/tracks/tracks.component').then(m => m.TracksComponent)
-    }
+    path: '',
+    loadComponent: () => import('./layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./modules/home/home.component').then(m => m.HomeComponent)
+      },
+      {
+        path: 'tracks',
+        loadComponent: () => import('./modules/tracks/tracks.component').then(m => m.TracksComponent)
+      }
+    ]
+  },
+  {
+    path: '',
+    loadComponent: () => import('./layouts/empty-layout/empty-layout.component').then(m => m.EmptyLayoutComponent),
+    children: [
+      {
+        path: 'auth',
+        loadComponent: () => import('./modules/auth/auth.component').then(m => m.AuthComponent)
+      }
+    ]
+  }
+
 ]; 
 
 export const appConfig: ApplicationConfig = {
